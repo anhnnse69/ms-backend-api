528e3c00341adc7708aa063a51148ba82ef451b8
DESKTOP-LA3EMHS\LENOVO <tuananh123123lon@gmail.com>
2026-03-15 12:24:55 +0700

    Create new feature admin-specialty-update

--- MSBackendAPI/MS.Application/DependencyInjection.cs
+++ MSBackendAPI/MS.Application/DependencyInjection.cs
@@ -29,6 +29,7 @@ using MS.Application.Services.ManagerServices.FacilitySpecialtyService;
 using MS.Application.Services.ManagerServices.PendingAppointmentPatientService;
 using MS.Application.Services.AdminServices.DeleteFacilityService;
 using MS.Application.Services.AdminServices.CreateSpecialtyService;
+using MS.Application.Services.AdminServices.UpdateSpecialtyService;
 
 namespace MS.Application
 {
@@ -76,6 +77,7 @@ namespace MS.Application
             services.AddScoped<IUpdateDoctorMedicalRecordService, UpdateDoctorMedicalRecordService>();
             services.AddScoped<IDeleteFacilityService, DeleteFacilityService>();
             services.AddScoped<ICreateSpecialtyService, CreateSpecialtyService>();
+            services.AddScoped<IUpdateSpecialtyService, UpdateSpecialtyService>();
             // Register MediatR pipeline behaviors for Data Annotation validation
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(DataAnnotationValidationBehavior<,>));
             return services;
---
+++ MSBackendAPI/MS.Application/Services/AdminServices/UpdateSpecialtyService/IUpdateSpecialtyService.cs
@@ -0,0 +1,15 @@
+﻿using MS.Application.Common.Response;
+
+namespace MS.Application.Services.AdminServices.UpdateSpecialtyService
+{
+    /// <summary>
+    /// Defines the contract for updating specialty information.
+    /// </summary>
+    public interface IUpdateSpecialtyService
+    {
+        /// <summary>
+        /// Processes the request to update a specialty.
+        /// </summary>
+        Task<ApiResponse<bool>> Process(Guid id, UpdateSpecialtyRequest request);
+    }
+}
