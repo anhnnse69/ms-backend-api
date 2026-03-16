f0460414ef10f34238c70870cc87e35d0f3b6e41
nhantruong7704 <nhantruong7704@gmail.com>
2026-03-16 21:07:45 +0700

    refactor code

--- MSBackendAPI/MS.Application/Services/PatientServices/CancelAppointmentService/CancelAppointmentRequest.cs
+++ MSBackendAPI/MS.Application/Services/PatientServices/CancelAppointmentService/CancelAppointmentRequest.cs
@@ -1,4 +1,5 @@
-﻿using MS.Domain.Enums.GeneralCodes;
+﻿using MS.Application.Common.Attributes;
+using MS.Domain.Enums.GeneralCodes;
 using System.ComponentModel.DataAnnotations;
 
 namespace MS.Application.Services.PatientServices.CancelAppointmentService
@@ -7,6 +8,9 @@ namespace MS.Application.Services.PatientServices.CancelAppointmentService
     {
         [Required(ErrorMessage = nameof(MessageCode.APP_MESSAGE_4003))]
         public Guid Id { get; set; }
+
+        [NoHtml(ErrorMessage = nameof(MessageCode.APP_MESSAGE_4024))]
+        [NoSqlInjection(ErrorMessage = nameof(MessageCode.APP_MESSAGE_4025))]
         public string Reason { get; set; }
     }
 }
