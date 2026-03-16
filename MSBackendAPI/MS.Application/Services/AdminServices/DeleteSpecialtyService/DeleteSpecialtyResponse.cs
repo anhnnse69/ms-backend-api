8877ba69ecac9f911b1fd160eeefb5cb9589306d
DESKTOP-LA3EMHS\LENOVO <tuananh123123lon@gmail.com>
2026-03-16 13:49:21 +0700

    Create new feature : admin-specialty-delete

---
+++ MSBackendAPI/MS.Application/Services/AdminServices/DeleteSpecialtyService/DeleteSpecialtyResponse.cs
@@ -0,0 +1,22 @@
+﻿namespace MS.Application.Services.AdminServices.DeleteSpecialtyService
+{
+    /// <summary>
+    /// Response model representing the result of specialty deletion
+    /// </summary>
+    public class DeleteSpecialtyResponse
+    {
+        /// <summary>
+        /// Indicates whether the specialty was successfully deleted.
+        /// </summary>
+        public bool IsDeleted { get; set; }
+
+        /// <summary>
+        /// Initializes response model
+        /// </summary>
+        /// <param name="isDeleted"></param>
+        public DeleteSpecialtyResponse(bool isDeleted)
+        {
+            IsDeleted = isDeleted;
+        }
+    }
+}
