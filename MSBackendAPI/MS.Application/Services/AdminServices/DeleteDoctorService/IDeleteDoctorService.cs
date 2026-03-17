215aa9aadbe25d6cd533cd3484112b39ca890635
DESKTOP-MAI3GAS\duype <duytkce181326@fpt.edu.vn>
2026-03-18 00:11:00 +0700

    Add new feature: soft delete doctor by admin

---
+++ MSBackendAPI/MS.Application/Services/AdminServices/DeleteDoctorService/IDeleteDoctorService.cs
@@ -0,0 +1,12 @@
+﻿using MS.Application.Common.Response;
+
+namespace MS.Application.Services.AdminServices.DeleteDoctorService
+{
+    /// <summary>
+    /// Interface for delete doctor service.
+    /// </summary>
+    public interface IDeleteDoctorService
+    {
+        Task<ApiResponse<DeleteDoctorResponse>> Process(Guid id);
+    }
+}
